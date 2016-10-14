761ca0ccaf2f5c9235047b1d356190cef6ecabea
MikeGoldsmith <goldsmith.mike@gmail.com>
2016-10-14 09:08:52 +0100

    add missing indexes on startup

--- src/try-cb-dotnet/App_Start/CouchbaseConfig.cs
+++ src/try-cb-dotnet/App_Start/CouchbaseConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using Couchbase;
 using Couchbase.Configuration.Client;
 
@@ -15,6 +16,51 @@ namespace try_cb_dotnet
             {
                 Servers = new List<Uri> { new Uri(couchbaseServer) }
             });
+
+            EnsureIndexes();
+        }
+
+        private static void EnsureIndexes()
+        {
+            var indexNames = new List<string>
+                {"def_sourceairport", "def_airportname", "def_type", "def_faa", "def_icao", "def_city"};
+
+            var bucket = ClusterHelper.GetBucket("travel-sample");
+            var result = bucket.Query<dynamic>("SELECT indexes.* FROM system:indexes WHERE keyspace_id = 'travel-sample';");
+
+
+            var foundIndexes = new List<string>();
+            var hasPrimary = false;
+            foreach (var row in result.Rows)
+            {
+                if (row.is_primary == "true")
+                {
+                    hasPrimary = true;
+                }
+                else
+                {
+                    foundIndexes.Add((string) row.name);
+                }
+            }
+
+            if (!hasPrimary)
+            {
+                bucket.Query<dynamic>("CREATE PRIMARY INDEX `def_primary` ON `travel-sample`;");
+            }
+
+            var missingIndexes = indexNames.Where(index => !foundIndexes.Contains(index)).ToList();
+            if (!missingIndexes.Any())
+            {
+                return;
+            }
+
+            foreach (var missingIndex in missingIndexes)
+            {
+                var propertyName = missingIndex.Replace("def_", string.Empty);
+                bucket.Query<dynamic>($"CREATE INDEX `{missingIndex}` ON `travel-sample`(`{propertyName}`);");
+            }
+
+            bucket.Query<dynamic>($"BUILD INDEX ON `travel-sample` ({string.Join(", ", missingIndexes)});");
         }
 
         public static void CleanUp()
