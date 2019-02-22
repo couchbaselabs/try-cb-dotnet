ac9cdd62df1a9b7281a1bc00268e59aa054d3a7f
Mike Goldsmith <goldsmith.mike@gmail.com>
2019-02-22 13:49:31 +0000

    make app use C#7.1 to use async Main

--- try-cb-dotnet/Program.cs
+++ try-cb-dotnet/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
 
 namespace try_cb_dotnet
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            await CreateWebHostBuilder(args).Build().RunAsync();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
